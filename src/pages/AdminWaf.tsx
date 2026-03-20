a273a0811cd37ab78a1dcfbdb77779a429451da6
Basoro <dentix.id@gmail.com>
2026-03-20 14:36:42 +0800

    Update

--- src/pages/AdminWaf.tsx
+++ src/pages/AdminWaf.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { DashboardLayout } from "@/components/DashboardLayout";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
